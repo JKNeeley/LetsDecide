f30f7f7edaa4295203a358d51b41ff0e47870e07
cjwzkb <cjwzkb@mail.missouri.edu>
2023-11-27 15:21:00 -0600

    review

--- backend/app.js
+++ backend/app.js
@@ -194,62 +194,6 @@ app.post('/api/forms', (req, res) => {
     res.redirect('/');
 });
 
-
-//Create Ballot
-app.post('/api/questions', (req, res) => {
-  var question_count = req.body.question_count;
-  var i = 0;
-  /*
-  Passing the questions through post request
-  Each element from the post request will be an array, this way we can pass in as many questions as we want
-  *Options will be a 2d array
-  */
-
-  while(i<question_count)
-  {
-    let question = new questionModel(
-      {
-        ID: req.body.id[i],
-        Parent_Form_ID: req.body.parent_form_id[i],
-        Type: req.body.type[i],
-        Description: req.body.description[i],
-        Show_Top: null,
-        Options: req.body.options[i]//array
-      });
-    questionModel.save()
-    .then(() => {
-      console.log('Data saved')
-    })
-    .catch((error) => {
-      console.log('Error saving data: ', error);
-    })
-    i++;
-
-  const finishedForm = new formModel({
-      Title: req.body.title,
-      Description: req.body.description,
-      Type: req.body.type,
-      State: req.body.state,
-      Time_Close: req.body.time_close,
-      Draft_Name: req.body.draft_name,
-      Draft_Code: req.body.draft_code,
-      Questions: req.body.questions,
-      Responses: req.body.responses,
-      Access: req.body.access
-  });
-
-
-    finishedForm.save()
-      .then(savedForm => {
-        res.status(201).json(savedForm); // Respond with the saved form data
-      })
-      .catch(err => {
-        res.status(500).send(err); // Handle error if form saving fails
-      });
-    }
-  });
-
-
 //Create Questions
 app.post('/api/questions', async (req, res) => {
   try {
