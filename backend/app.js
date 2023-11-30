336b98cdccbad823815e40843a461492d38d11a5
Mel Tully <melissa.j.tulls@gmail.com>
2023-11-30 17:40:58 -0600

    result route w/ model wip; count fn w/ all options

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,8 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 const formModel = require('./models/form')
 const questionModel = require('./models/question')
 const responseModel = require('./models/response')
-const accessModel = require('./models/access');
+const accessModel = require('./models/access')
+const resultsModel = require('./models/results')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
@@ -58,7 +59,7 @@ app.get('/src/app/temp-save-access', (req, res) => {
 /// Define routes here ///
 
 
-// GET
+// GET Requests //
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
@@ -90,84 +91,110 @@ app.get('/api/forms/:id', (req, res)=>{
   })
 })
 
-// Returns the results of a form of a specific ID
+/*results psuedocode
+get form info
+get question info
+get response info
+get counts for each question
+get winners for each question
+assign Results.title, .description
+assign Results.Question[forEach]
+  description
+  winners
+  count
+*/
+
+// Returns the Results of a form of a specific ID
 // WIP, only supports First-Past-The-Post
 app.get('/api/forms/result/:id', (req, res)=>{
   formModel.findById(req.params.id).then(document=>{ //find form
-    if (document == null){
-      res.status(204).send('No document with this ID located')
-    }
-    /*
-    else if (document.State != 2) //check if form has closed
-    {
-      res.send('Vote has not yet concluded')
-    }
-    */
-    else{
-      responseModel.findById(document.Responses_ID).then(resp=>{ //find responses
-        if (resp == null){
-          res.status(204).send('No document with this ID located')
-        }
-        else{
-          let answers = []
-          //resp.Responses.forEach(to_arr)
-          for (let i = 0; i < resp.Responses.length; i++){
-            //console.log(resp.Responses[i].Answers)
-            answers.push(resp.Responses[i].Answers)
-          }
-          console.log(answers)
-          let count = countVotes(answers)
-          res.status(200).send(count)
-        }
-      })
-    }
-  })
-  .catch((error) => {
-    console.log('Error fetching data: ', error);
-  })
+    if (document == null){ res.status(204).send('No form document with this ID located').end() }
+    //check if form is closed
+    // if (document.State != 2){ res.send('Vote has not yet concluded').end() }
+    responseModel.findById(document.Responses_ID).then(resp=>{ //find responses
+      if (resp == null){ res.status(204).send('No response document with this ID located').end() }
+      //check for 0 responses
+      //todo
+      questionModel.findById(document.Questions_ID).then(quest=>{ //find questions
+        if (quest == null){ res.status(204).send('No question document with this ID located').end() }
 
+        // count answers
+        let answers = []
+        for (let i = 0; i < resp.Responses.length; i++){
+          answers.push(resp.Responses[i].Answers)
+        }
+        //console.log('answers')
+        //console.log(answers)
+        let count = countVotes(answers, quest.Questions)
+
+        // get winners from count
+        //let winners = getWinners(quest, count)
+
+        // create results instance
+        const results = new resultsModel;
+          //populate
+
+        // send results
+        res.status(200).send(results);
+      })
+    })
+  })
+  .catch((error) => { console.log('Error fetching data: ', error); })
 })
 
-function to_arr(x){
-  //console.log(x)
-  delete x.Parent_Form_ID
-  answers.push(x.Answers)
-}
+// Helper functions for Results
 
-// Currently assumes First-Past-The-Post
-function countVotes(ans)
-{
-  count = []
+//Currently assumes First-Past-The-Post
+function countVotes(ans, quest){
+  // traverse questions, create 2nd array
+  let count = []
+  for (let i = 0; i < quest.length; i++){
+    let options = []
+    for (let j = 0; j < quest[i].Options.length; j++){
+      //console.log('testing')
+      //console.log(quest[i].Options[j])
+      options.push({option:quest[i].Options[j], votes:0})
+    }
+    //console.log('options')
+    //count.push(options)
+  }
+
+  //console.log('count')
+  //console.log(count)
+
   for (let i = 0; i < ans[0].length; i++){
-    //console.log('i')
-    count.push([])
     for (let j = 0; j < ans.length; j++){
-        //console.log('j')
-        if (ans[j][i].length > 1){
-            break
-        }
+        if (ans[j][i].length > 1){ break } // FPTP check
         let vote = ans[j][i][0]
         let voteFound = false
+        console.log('test')
+        console.log(count[i])
         for (let k = 0; k < count[i].length; k++){
-            if (count[i][k][0] == vote){
+            if (count[i][k].option == vote){
                 voteFound = true
-                count[i][k][1] += 1
+                count[i][k].votes += 1
                 break
             }
         }
-        if (!voteFound){
-            count[i].push([vote,1])
-            //console.log('no')
-        }
+        if (!voteFound){ count[i].push({option:vote,votes:1}) } // just in case
     }
   }
+
+  for (let i = 0; i < count.length; i++){
+    count[i].sort(function(a,b){return b.votes - a.votes})
+  }
+
+  console.log('returning')
   console.log(count)
   return count
 }
 
+function getWinners(quest, count){
+  return null
+}
 
 
-// POST
+// POST Requests //
 
 //Create Vote Form
 app.post('/api/forms', (req, res) => {
@@ -185,14 +212,14 @@ app.post('/api/forms', (req, res) => {
       Responses: req.body.responses,//array
       Accesses: req.body.accesses//array
     });
-    finishedForm.save()
-    .then(() => {
-      console.log('Data saved')
-    })
-    .catch((error) => {
-      console.log('Error saving data: ', error);
-    })
-    res.redirect('/');
+  finishedForm.save()
+  .then(() => {
+    console.log('Data saved')
+  })
+  .catch((error) => {
+    console.log('Error saving data: ', error);
+  })
+  res.redirect('/');
 });
 
 //Create Questions
