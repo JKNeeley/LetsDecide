0b3a91d3c6bbb614cc3e5d64682eb8b28be81f4c
cjwzkb <cjwzkb@mail.missouri.edu>
2023-12-05 13:10:25 -0600

    edited cors()

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.use(cors);
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../frontend/src')));
 
